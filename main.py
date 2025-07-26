51039a91b5717c538731f8ca974e291c55017eee
Amir Aziz <22pwdsc0062@uetpeshawar.edu.pk>
2025-07-26 23:32:27 +0500

    Update main.py

--- main.py
+++ main.py
@@ -140,3 +140,11 @@ class Login(BaseModel):
     password: str
 
 # JWT Bearer Security
+@app.post("/login", tags=["Authentication"])
+async def login(user: Login):
+    db_user = fake_users_db.get(user.username)
+    if not db_user or db_user["password"] != user.password:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
+    token_data = {"sub": user.username}
+    token = create_jwt_token(token_data)
+    return {"access_token": token, "token_type": "bearer"}
