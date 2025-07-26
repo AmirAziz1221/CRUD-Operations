041a8c4fecf3e119bd4e2228b06b7f16fbd99493
Amir Aziz <22pwdsc0062@uetpeshawar.edu.pk>
2025-07-26 23:09:44 +0500

    updating

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI, HTTPException, status, Query
+from fastapi import FastAPI, HTTPException, status, Query, Depends, Request
 from pydantic import BaseModel, Field
 from typing import List
+import time
+import asyncio
+
 
 app = FastAPI()
 
@@ -19,23 +22,23 @@ class Book(BaseModel):
 
 books = []
 
-@app.post("/books/", response_model=Book)
+@app.post("/books/", response_model=Book, tags=["Books"])
 async def create_book(book : Book):
     books.append(book)
     return book
 
-@app.get("/books/", response_model=List[Book])
+@app.get("/books/", response_model=List[Book], tags=["Books"])
 async def get_books():
     return books
 
-@app.get("/books/{book_title}", response_model=Book)
+@app.get("/books/{book_title}", response_model=Book, tags=["Books"])
 async def get_book(book_title:str):
     for book in books:
         if book.title == book_title:
             return book
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = "Book not found")
     
-@app.put("/books/{book_title}", response_model=Book)
+@app.put("/books/{book_title}", response_model=Book, tags=["Books"])
 async def update_book(book_title: str, book:Book):
     for index, existing_book in enumerate(books):
         if existing_book.title == book_title:
@@ -44,7 +47,7 @@ async def update_book(book_title: str, book:Book):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = "Book not found")
     
 
-@app.delete("/books/{book_title}", response_model=dict)
+@app.delete("/books/{book_title}", response_model=dict, tags=["Books"])
 async def delete_book(book_title: str):
     for index, book in enumerate(books):
         if book.title == book_title:
@@ -52,7 +55,49 @@ async def delete_book(book_title: str):
             return {"message": "Book deleted successfully"}
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
 
-@app.get("/")
+@app.get("/", tags=["Root"])
 async def read_books(limit: int = Query(10, ge=1, le=100), published_after: int = Query(2000, ge=1900, le=2025)):
     filtered_books = [book for book in books if books.published_year >= published_after]
     return filtered_books[:limit]
+
+# Sync and Async Tasks
+
+@app.get("/sync-task/", tags=["Sync"])
+def sync_task():
+    time.sleep(5)
+    return {"message": "sync task completed"}
+
+@app.get("/async-task/", tags=["Async"])
+async def async_task():
+    await asyncio.sleep(5)  # Non-blocking, async task
+    return {"message": "Async task completed"}
+
+
+async def log_request(request: Request):
+    print(f"Request path: {request.url.path}")
+    return {"path": request.url.path}
+
+
+@app.get("/log/", dependencies=[Depends(log_request)], tags=["Logging"])
+async def log_route():
+    return {"message": "Logging request path"}
+
+
+# Simulaed Database Dependency 
+class FackeDBSession:
+    def __init__(self):
+        self.connection = "Fake DB Connection"
+    
+    def close(self):
+        print("Closing fake DB connection")
+
+async def get_db():
+    db = FackeDBSession()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/items/", tags = ["FakeDB"])
+async def read_items(db: FackeDBSession = Depends(get_db)):
+    return {"db_connection": db.connection, "message": "Items fetched successfully"}
